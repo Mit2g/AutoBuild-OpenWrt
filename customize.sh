243647d2aafac793e21054138d7b2453506a0cdf
beckball <50163397+beckball@users.noreply.github.com>
2020-04-17 01:01:20 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,6 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+#2. Switch default Language to Simple Chinese
+option zh_cn '普通话 (Chinese)' openwrt/feeds/luci/modules/luci-base/root/etc/config/luci
+
